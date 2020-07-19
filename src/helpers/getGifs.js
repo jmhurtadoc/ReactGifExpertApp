7e7f0e209e7f196fe2da05c5c8970398f8196d19
Jhader <jmhurtadoc@unal.edu.co>
2020-07-20 01:18:09 +0800

    se corrige href final

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async ( category ) => {
     const apiKey = "Z3uLD82ic3zFDUtmsm2rix6IQ5PmK4Q1";
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${ encodeURI( category )}&limit=10&offset=0&lang:=es`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${ encodeURI( category )}&limit=10&offset=0&lang:=es`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map((i) => {
